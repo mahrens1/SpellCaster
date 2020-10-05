2c7b3824bc8de794c4fedaafe2284308fee864d7
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-10-05 18:18:17 -0500

    added character controller
    Still need to make character move

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    //Variables
+    
+
+    //Methods
+    protected void LeftMovement()
+    {
+        
+    }
+
+    protected void RightMovement()
+    {
+
+    }
+
+    protected void ShootSpell()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10;
+
+    private Vector2 playerInput;
+
+    private Rigidbody characterRig;
+    private Collider characterCollider;
+
+
+    void Start()
+    {
+        characterRig = GetComponent<Rigidbody>();
+        characterCollider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(playerInput.x, 0, playerInput.y);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inputDirection.x = speed;
+        }
+
+        characterRig.AddForce((inputDirection * Time.deltaTime) , ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //Methods
+    void LeftMovement()
+    {
+        characterRig.AddForce(new Vector3(speed, 0, 0), ForceMode.Impulse);
+    }
+
+    void RightMovement()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void ShootSpell()
+    {
+
+    }
+}

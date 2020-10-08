9b9c4cf4b874516fb307b4f32c3afddca6f0c848
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-10-08 17:41:15 -0500

    Renamed PLayer to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField]float speed = 10f;
     [SerializeField]float jumpForce = 10f;

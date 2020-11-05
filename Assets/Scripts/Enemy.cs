3cc0a58266924ba1f4b46d6aa3c5cd2ff4eeb625
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-11-05 12:20:16 -0600

    added enemy sctipt for taking damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float maxHealth;
+    public float curHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+
+        if(curHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Spell")
+        {
+            curHealth--;
+        }
+    }
+
+}

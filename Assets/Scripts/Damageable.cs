5ed9e83d436f015f88a1d979b0ab61e9a6609f71
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-11-09 12:55:46 -0600

    Player can now destroy damageable objects

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField] float health;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+    }
+}

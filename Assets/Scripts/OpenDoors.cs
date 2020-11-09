0762f5a1df1ae59da9bfdf759a73af5ef87d170e
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-11-09 15:20:10 -0600

    Added enemyTrigger

---
+++ Assets/Scripts/OpenDoors.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoors : MonoBehaviour
+{
+    bool enemiesPresent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enemiesPresent)
+        {
+            
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            enemiesPresent = true;
+        }
+        else
+        {
+            enemiesPresent = false;
+        }
+    }
+}

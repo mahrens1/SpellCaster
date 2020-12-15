8dda57a784e37b465425de27efcd39e68f3064c8
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-12-15 16:35:19 -0600

    s

--- Assets/Scripts/PlayerControllerV2.cs
+++ Assets/Scripts/PlayerControllerV2.cs
@@ -15,9 +15,11 @@ public class PlayerControllerV2 : MonoBehaviour
     private new Collider collider;
     private bool isOnGround;
     public float curHealth;
+    public bool isDead;
     private Animator animator;
     private readonly int movementAnimParam = Animator.StringToHash("movementInput");
     private readonly int jumpAnimParam = Animator.StringToHash("jumpInput");
+    private readonly int deathAnimParam = Animator.StringToHash("isDead");
 
     [SerializeField] [Tooltip("0 = No Turning, 1 = Instant Snap")] [Range(0, 1)] private float turnspeed = 0.1f;
     
@@ -28,31 +30,20 @@ public class PlayerControllerV2 : MonoBehaviour
         collider = GetComponent<Collider>();
         animator = GetComponentInChildren<Animator>();
 
+        Cursor.visible = false;
+
         isOnGround = true;
     }
 
     private void FixedUpdate()
     {
-        var inputDirection = new Vector3(input.x, 0, input.y);
+        Vector3 cameraRelativeInputDirection = GetCameraRelativeInputDirection();
 
-        Vector3 cameraFlattenedForward = Camera.main.transform.forward;
-        cameraFlattenedForward.y = 0;
-        var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
+        UpdatePhysicsMaterial();
 
-        Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
+        Move(cameraRelativeInputDirection);
 
-        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicsMaterial;
-
-        if (playerRb.velocity.magnitude < maxSpeed)
-        {
-            playerRb.AddForce(cameraRelativeInputDirection * accelerationForce, ForceMode.Acceleration);
-        }
-
-        if (cameraRelativeInputDirection.magnitude > 0)
-        {
-            var targetRotation = Quaternion.LookRotation(cameraRelativeInputDirection);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnspeed);
-        }
+        RotateToFaceInputDirection(cameraRelativeInputDirection);
 
         //Jump
         if (Input.GetKey(KeyCode.Space) && isOnGround)
@@ -63,6 +54,57 @@ public class PlayerControllerV2 : MonoBehaviour
 
         //JumpInput();
 
+    }
+
+    /// <summary>
+    /// Turning the character to face the direction it wants to move in.
+    /// </summary>
+    /// <param name="movementDirection"> The direction the character is trying to move in. </param>
+    private void RotateToFaceInputDirection(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude > 0)
+        {
+            var targetRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnspeed);
+        }
+    }
+
+    /// <summary>
+    /// Moves the player in a direction based on its max speed.
+    /// </summary>
+    /// <param name="moveDirection"> The direction to move in. </param>
+    private void Move(Vector3 moveDirection)
+    {
+        if (playerRb.velocity.magnitude < maxSpeed)
+        {
+            playerRb.AddForce(moveDirection * accelerationForce, ForceMode.Acceleration);
+        }
+    }
+
+    /// <summary>
+    /// Updates physics material to a low friction option if player is moving 
+    /// or a high friction option if trying to stop.
+    /// </summary>
+    private void UpdatePhysicsMaterial()
+    {
+        collider.material = input.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicsMaterial;
+    }
+
+    /// <summary>
+    /// Uses the input of Vector to create camera relative version so the player can move based on the camera.
+    /// </summary>
+    /// <returns> cameraRelativeInputDirection. </returns>
+    private Vector3 GetCameraRelativeInputDirection()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+
+        Vector3 cameraFlattenedForward = Camera.main.transform.forward;
+        cameraFlattenedForward.y = 0;
+        var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
+
+        Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
+
+        return cameraRelativeInputDirection;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -88,13 +130,21 @@ public class PlayerControllerV2 : MonoBehaviour
         movementInput();
     }
 
+    public void PlayerDeath()
+    {
+        if(curHealth <= 0)
+        {
+            isDead = true;
+            DeathInput();
+        }
+    }
+
     /// <summary>
     /// Changes the players run and idle animation.
     /// </summary>
     public void movementInput()
     {
         animator.SetFloat(movementAnimParam, input.magnitude);
-
     }
 
     /// <summary>
@@ -102,7 +152,11 @@ public class PlayerControllerV2 : MonoBehaviour
     /// </summary>
     public void JumpInput()
     {
-
         animator.SetFloat(jumpAnimParam, input.y);
+    }
+
+    public void DeathInput()
+    {
+        animator.SetBool(deathAnimParam, isDead);
     }
 }
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public Vector2 _move;
+    public Vector2 _look;
+    public float aimValue;
+    public float fireValue;
+
+    public Vector3 nextPosition;
+    public Quaternion nextRotation;
+
+    public float rotationPower = 3f;
+    public float rotationLerp = 0.5f;
+
+    public float speed = 1f;
+    public Camera camera;
+    public Rigidbody followTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(_look.x * rotationPower, Vector3.up);
+
+        followTransform.transform.rotation *= Quaternion.AngleAxis(_look.y * rotationPower, Vector3.up);
+
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        //Clamp the Up/down rotation
+
+    }
+}

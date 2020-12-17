8c7a292580130832f347975b5bb1acb455c5fb2c
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-12-17 10:03:27 -0600

    Worldfire added

---
+++ Assets/Scripts/WorldFire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldFire : MonoBehaviour
+{
+
+    public int damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            var player = other.gameObject.GetComponent<PlayerControllerV2>();
+
+            player.curHealth -= damage;
+        }
+    }
+}
